187fd051ad26216a88e4220fb980156321275196
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-06-16 16:56:23 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,14 +10,12 @@ def main():
     args = sys.argv[4:]
 
     with TemporaryDirectory() as tmpdir:
+        unshare_command = ["unshare", "--pid", "--fork", "--mount-proc", "--"]
+        chroot_command = ["chroot", tmpdir, os.path.join("/", os.path.basename(command)), *args]
+
         shutil.copy(command, tmpdir)
         completed_process = subprocess.run(
-            [
-                "chroot",
-                tmpdir,
-                os.path.join("/", os.path.basename(command)),
-                *args
-            ],
+            unshare_command + chroot_command,
             capture_output=True,
         )
         sys.stdout.buffer.write(completed_process.stdout)
