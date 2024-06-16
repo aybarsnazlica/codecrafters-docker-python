cb0b8c43e2b0315f2980bd25d17317a9e3569545
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-06-16 16:47:44 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
+import os
+import shutil
 import subprocess
 import sys
+from tempfile import TemporaryDirectory
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    sys.stdout.buffer.write(completed_process.stdout)
-    sys.stderr.buffer.write(completed_process.stderr)
-    sys.exit(completed_process.returncode)
+    with TemporaryDirectory() as tmpdir:
+        shutil.copy(command, tmpdir)
+        completed_process = subprocess.run(
+            [
+                "chroot",
+                tmpdir,
+                os.path.join("/", os.path.basename(command)),
+                *args
+            ],
+            capture_output=True,
+        )
+        sys.stdout.buffer.write(completed_process.stdout)
+        sys.stderr.buffer.write(completed_process.stderr)
+        sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
